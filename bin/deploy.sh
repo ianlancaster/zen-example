a11a1cf306510607fe58e980f9aad74d6b007d05
Ian Lancaster <ianclancaster@gmail.com>
2017-05-05 17:37:26 -0600

    Updated deploy script

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 if git diff-index --quiet HEAD --; then # Check to make sure there are no uncommited changes
     set -o errexit; # Exit on error
-echo Step 1/1: Creating production build;
+echo Step 1/4: Creating production build;
     npm run deploy:prod;
-echo Step 2/2: Archiving previous production image;
+echo Step 2/4: Archiving previous production image;
     now=$(date +"%m_%d_%Y") # Tag and archive old docker latest. Push to dockerhub.
     docker tag ianlancaster/zen-production:latest ianlancaster/zen-production:$now;
     docker push ianlancaster/zen-production:$now;
-echo Step 3/3: Creating new production image;
+echo Step 3/4: Creating new production image;
     mv Dockerfile.prod.off Dockerfile;
     docker build -t ianlancaster/zen-production . # Build a new latest docker image. Push to dockerhub.
     mv Dockerfile Dockerfile.prod.off;
